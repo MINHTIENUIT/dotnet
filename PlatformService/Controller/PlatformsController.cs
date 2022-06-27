d712f021085a52fccfebc0560fd804b3d560cd70
Văn Minh Tiến <vanminhtien96@gmail.com>
2022-06-27 13:57:09 +0700

    Add existing project files to Git

---
+++ PlatformService/Controller/PlatformsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
+namespace PlatformService.Controller
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PlatformsController : ControllerBase
+	{
+		private readonly IPlatformRepo _repository;
+		private readonly IMapper _mapper;
+
+		public PlatformsController(IPlatformRepo respository, IMapper mapper)
+		{
+			_repository = respository;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<PlatformReadDto>> getPlatforms()
+        {
+			Console.WriteLine("--> Get platforms");
+			var platforms = _repository.GetAllPlatforms();
+			return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+	}
+}
+
